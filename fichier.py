428970b668063c62f7dada508c3622bc24683586
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2024-01-24 11:49:29 +0100

    py

---
+++ fichier.py
@@ -0,0 +1,17 @@
+import serial
+
+ser = serial.Serial('COMx', 9600)  # Remplacez 'COMx' par le port série approprié sur votre PC
+
+# Attendre le début de la transmission
+while ser.readline().decode('utf-8').strip() != '{"averageTemperature":':
+    pass
+
+# Ouvrir un fichier pour sauvegarder les données JSON
+with open('data_from_arduino.json', 'w') as file:
+    while True:
+        line = ser.readline().decode('utf-8').strip()
+        if line == '}':
+            break
+        file.write(line + '\n')
+
+ser.close()
