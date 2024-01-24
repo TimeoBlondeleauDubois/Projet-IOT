bc49e7614e68cc934c009934b35ac74b7b1c3e66
Guenole <guenole.legrand@viacesi.fr>
2024-01-24 08:52:27 +0100

    meteo

--- programme.c
+++ programme.c
@@ -1,11 +1,21 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
+/* Include libraries of BME280 sensor */
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+/*#include <SPI.h>  // uncomment his if you are using SPI interface
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
+
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -13,26 +23,80 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+
 void setup() {
+  Serial.begin(9600);
+
   Wire.pins(0,2);
   Wire.begin();
-  Serial.begin(115200);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
   delay(2000);
-  display.clearDisplay();
 
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  // Display static text
-  display.println("Hello, world!");
-  display.display(); 
+  bool status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not detect a BME280 sensor, Fix wiring Connections!");
+    while (1);
+  }
+
+  Serial.println("-- Print BME280 readings--");
+  Serial.println();
 }
 
-void loop() {
+
+void loop() { 
+
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
   
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
+  
+  Serial.print("Pression = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Humidite = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
+
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.println("METEO");
+  display.println();
+
+  display.print("Temp. = ");
+  display.print(bme.readTemperature());
+  display.println(" C");
+
+  display.print("Press. = ");
+  display.print(bme.readPressure() / 100.0F);
+  display.println(" hPa");
+
+  display.print("Hum. = ");
+  display.print(bme.readHumidity());
+  display.println(" %");
+
+  display.display();
+
+  delay(5000);
 }
